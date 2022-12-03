89cd33a5f081fa1b011f519d1fe9f4bc8d4ef98f
Jakob Pettersson Peeker <jakob.peeker@eigentech.com>
2022-12-03 09:47:59 +0000

    Day 3 Part 1 done

--- aoc/day_03/__main__.py
+++ aoc/day_03/__main__.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from utils.decorators import time_it
 from utils.helper_functions import (
     read_input,
@@ -12,9 +14,48 @@ INPUT_FILENAME = "aoc/day_03/INPUT.txt"
 EXAMPLE_FILENAME = "aoc/day_03/EXAMPLE_01.txt"
 
 
+# def split_sentences(lines):
+#     temp = []
+#     for line in lines:
+#         hl = int(len(line)/2)
+#         temp.append([line[:hl], line[:hl]])
+#     return temp
+
+def get_value(c: str):
+    if c.islower():
+        return ord(c) - 96
+    else:
+        return ord(c) - 38
+
+
+def get_duplicate(a: str, b: str):
+
+    for char_a in a:
+        if char_a in b:
+            return char_a
+
+    for char_b in b:
+        if char_b in a:
+            return char_b
+
+    return None
+
+
+def solve_lines_1(lines: List[str]):
+    split_lines = [[line[:int(len(line)/2)], line[int(len(line)/2):]] for line in lines]
+    total = 0
+    for line in split_lines:
+        dup = get_duplicate(line[0], line[1])
+        value = get_value(dup)
+        total += value
+    return total
+
 @time_it
 def solve_part_1():
     print("Day 03 - Part 1")
+    lines = read_input_lines(INPUT_FILENAME, True)
+    total = solve_lines_1(lines)
+    print(f"The sum of priorities for duplicate items is {total}")
 
 
 @time_it
