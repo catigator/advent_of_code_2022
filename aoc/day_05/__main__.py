ad8ad405a334146ffc797471debc088b571b89a4
Jakob Pettersson Peeker <jakob.peeker@eigentech.com>
2022-12-05 20:46:00 +0000

    Solved Day 5 Part 1

--- aoc/day_05/__main__.py
+++ aoc/day_05/__main__.py
@@ -1,3 +1,4 @@
+import re
 from collections import defaultdict
 from typing import List
 
@@ -27,21 +28,56 @@ def get_rows(lines):
 
 def get_stacks(rows):
     stacks = defaultdict(list)
-    for i_row in range(len(rows)):
+    for i_row in range(len(rows) -1, -1, -1):
         for i_stack, char in enumerate(rows[i_row]):
-            stacks[i_stack].append(char)
+            if char != " ":
+                stacks[i_stack].append(char)
     return stacks
+
+
+def move_from_a_to_b(quantity: int, a: int, b: int):
+    for i in range(quantity):
+        to_move = a[-1]
+        b += to_move
+        a = a[:-1]
+
+    return a, b
+
+
+def parse_command(command):
+    new_command = re.findall('[0-9]+', command)
+    new_command = [int(num) for num in new_command]
+    return new_command
+
+
+def handle_command(command, stacks):
+    a_i = command[1] - 1
+    b_i = command[2] - 1
+    quantity = command[0]
+
+    stacks[a_i], stacks[b_i] = move_from_a_to_b(quantity, stacks[a_i], stacks[b_i])
+
+    return stacks
+
+
+def handle_commands(commands, stacks):
+    for command in commands:
+        stacks = handle_command(command, stacks)
 
 @time_it
 def solve_part_1():
-    lines = read_input_lines_strip_newline(EXAMPLE_FILENAME)
+    lines = read_input_lines_strip_newline(INPUT_FILENAME)
     split_lines = split_list_on_entry(lines, "")
     starting_stacks = [line for line in split_lines[0] if line[1] != "1"]
     commands = split_lines[1]
     rows = get_rows(starting_stacks)
     stacks = get_stacks(rows)
-    print(lines)
+    commands = [parse_command(command) for command in commands]
+    handle_commands(commands, stacks)
+
+    top_crates = "".join([stack[-1] for key, stack in stacks.items()])
     print(stacks)
+    print(f"The top crates are {top_crates}")
     print("Day 05 - Part 1")
 
 
