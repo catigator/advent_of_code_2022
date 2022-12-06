e9def61eb187fae4b3000eca0484e3ddec9d635c
Jakob Pettersson Peeker <jakob.peeker@eigentech.com>
2022-12-06 08:41:55 +0000

    Solved Day 6 part 2!

--- aoc/day_06/__main__.py
+++ aoc/day_06/__main__.py
@@ -14,17 +14,17 @@ INPUT_FILENAME = "aoc/day_06/INPUT.txt"
 EXAMPLE_FILENAME = "aoc/day_06/EXAMPLE_01.txt"
 
 
-def process_lines(lines: List[str]):
+def process_lines(lines: List[str], num_chars=4):
     first_markers = []
     for line in lines:
-        first_markers.append(get_first_marker(line))
+        first_markers.append(get_first_marker(line, num_chars))
     return first_markers
 
 
-def get_first_marker(line):
-    for i in range(3, len(line)):
-        sequence = set(line[i-3:i+1])
-        if len(sequence) == 4:
+def get_first_marker(line, num_chars=4):
+    for i in range(num_chars-1, len(line)):
+        sequence = set(line[i-num_chars+1:i+1])
+        if len(sequence) == num_chars:
             return i + 1
     return None
 
@@ -40,8 +40,11 @@ def solve_part_1():
 @time_it
 def solve_part_2():
     print("Day 06 - Part 2")
+    lines = read_input_lines(INPUT_FILENAME)
+    first_markers = process_lines(lines, 14)
+    print(f"The first markers were {first_markers}")
 
 
 if __name__ == "__main__":
     solve_part_1()
-    # solve_part_2()
+    solve_part_2()
