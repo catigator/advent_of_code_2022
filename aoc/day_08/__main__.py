32d52a9e19e32c612c39470bc8e6aeb63f58fdc4
Jakob Pettersson Peeker <jakob.peeker@eigentech.com>
2022-12-10 00:10:39 +0000

    Day 8 start

--- aoc/day_08/__main__.py
+++ aoc/day_08/__main__.py
@@ -12,9 +12,33 @@ INPUT_FILENAME = "aoc/day_08/INPUT.txt"
 EXAMPLE_FILENAME = "aoc/day_08/EXAMPLE_01.txt"
 
 
+def get_row_positions(y_start, y_end, x_start, x_end):
+    y = range(y_start, y_end + 1)
+    x = range(x_start, x_end + 1)
+
+    positions = [[(py, px) for py in y] for px in x]
+    return positions
+
+
+def get_visibility(matrix, visible):
+    for y in range(len(matrix)):
+        for x in range(len(matrix)):
+            ylen = len(matrix)
+            xlen = len(matrix[0])
+            row_positions_vert = get_row_positions(y, y, 0, xlen)
+            row_positions_hori = get_row_positions(0, ylen, x, x)
+            a = 1
+
+
+
 @time_it
 def solve_part_1():
     print("Day 08 - Part 1")
+    matrix = read_input_int_matrix(EXAMPLE_FILENAME)
+    visible = [[0] * len(matrix)] * len(matrix[0])
+    print(matrix)
+    print(visible)
+    get_visibility(matrix, visible)
 
 
 @time_it
