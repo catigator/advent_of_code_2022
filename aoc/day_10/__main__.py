dc6792a1e60a6db87d5a16ac55480a91a6826873
Jakob Pettersson Peeker <jakob.peeker@eigentech.com>
2022-12-11 20:42:36 +0000

    Boutta refactor a lot

--- aoc/day_10/__main__.py
+++ aoc/day_10/__main__.py
@@ -9,43 +9,67 @@ from utils.helper_functions import (
 
 
 INPUT_FILENAME = "aoc/day_10/INPUT.txt"
-EXAMPLE_FILENAME = "aoc/day_10/EXAMPLE_01.txt"
+EXAMPLE_FILENAME = "aoc/day_10/EXAMPLE_02.txt"
 
 def process_lines(split_lines):
     cycle = 0
     adds = []
+    to_delete = []
+    signal_strengths = []
+    x_vals = []
+    vals = []
     x = 1
     for line in split_lines:
         cycle += 1
         command = line[0]
+        x_vals.append(x)
+
+        if cycle % 20 == 0:
+            signal_strengths.append(cycle*x)
+
         if command != "noop":
+            val = int(line[1])
+
+            vals.append(val)
             if command == "addx":
-                num = int(line[1])
+                num = int(val)
                 adds.append((cycle+2, num))
-        to_delete = []
+        else:
+            vals.append(0)
         for i, add in enumerate(adds):
             if add[0] == cycle:
                 x += add[1]
                 to_delete.append(i)
         for i in to_delete:
-            adds = adds.pop(i)
+            adds.pop(i)
+        to_delete = []
     print(f"X is {x} after {cycle} cycles")
 
-    for just_looping_through_the_last_cycles in range(3):
+    to_delete = []
+
+    for just_looping_through_the_last_cycles in range(221 - len(split_lines)):
         cycle += 1
+        x_vals.append(x)
+        if cycle % 20 == 0:
+            signal_strengths.append(cycle*x)
         for i, add in enumerate(adds):
             if add[0] == cycle:
                 x += add[1]
                 to_delete.append(i)
+        for i in to_delete:
+            adds.pop(i)
+        to_delete = []
 
-    return x, cycle
+    return x, cycle, signal_strengths
+
 
 @time_it
 def solve_part_1():
     print("Day 10 - Part 1")
     lines = read_input_lines(EXAMPLE_FILENAME)
     split_lines = [line.split() for line in lines]
-    x, cycle = process_lines(split_lines)
+    x, cycle, signal_strengths = process_lines(split_lines)
+    print(f"The signal strength total is {sum(signal_strengths)}")
     print(x, cycle)
 
 
