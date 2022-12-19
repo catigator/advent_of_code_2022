80547ea3df2f339c7a321335fffd367832c4c45b
Jakob Pettersson Peeker <jakob.peeker@eigentech.com>
2022-12-19 12:17:41 +0000

    Code changes but part 1 still working

--- aoc/day_12/__main__.py
+++ aoc/day_12/__main__.py
@@ -40,7 +40,7 @@ def in_range(pos, matrix):
         return False
 
 
-def calculate_possible_moves(matrix):
+def calculate_possible_moves(matrix, part=1):
     ylen = len(matrix)
     xlen = len(matrix[0])
     possible_moves = [ [0]*xlen for i in range(ylen)]
@@ -53,7 +53,10 @@ def calculate_possible_moves(matrix):
             for key, move in MOVES.items():
                 new_pos = add_pos(pos, move)
                 if in_range(new_pos, matrix):
-                    possible_move = is_possible_move(pos, new_pos, matrix)
+                    if part == 1:
+                        possible_move = is_possible_move(pos, new_pos, matrix)
+                    else:
+                        possible_move = is_possible_move_2(pos, new_pos, matrix)
                     if possible_move:
                         if possible_moves[y][x]:
                             possible_moves[y][x].append(key)
@@ -63,18 +66,20 @@ def calculate_possible_moves(matrix):
 
 
 def get_start_pos(matrix) -> Optional[Tuple]:
-    return get_pos_for_char(matrix, "S")
+    return get_pos_for_char(matrix, "S")[0]
 
 
 def get_end_pos(matrix) -> Optional[Tuple]:
-    return get_pos_for_char(matrix, "E")
+    return get_pos_for_char(matrix, "E")[0]
+
 
 def get_pos_for_char(matrix, char) -> Optional[Tuple]:
+    positions = []
     for y in range(len(matrix)):
         for x in range(len(matrix[y])):
             if matrix[y][x] == char:
-                return (y, x)
-    return None
+                positions.append((y, x))
+    return positions
 
 
 def get_value(char: str):
@@ -92,6 +97,12 @@ def is_possible_move(a, b, matrix):
     value_a = get_value(matrix[a[0]][a[1]])
     value_b = get_value(matrix[b[0]][b[1]])
     return value_b <= value_a + 1
+
+
+def is_possible_move_2(a, b, matrix):
+    value_a = get_value(matrix[a[0]][a[1]])
+    value_b = get_value(matrix[b[0]][b[1]])
+    return value_a <= value_b + 1
 
 
 def get_min_cost_from_list(possible_paths: List[int]):
@@ -148,6 +159,52 @@ def get_best_path(possible_moves, matrix, pos, goal, cost=0, lowest_costs=None):
         return None, lowest_costs
 
 
+def get_best_path_2(possible_moves, matrix, pos, goal, cost=0, lowest_costs=None):
+    """
+    returns best_path, cost, lowest_costs
+    """
+
+    # 1. Starting from S
+    # 2. Go through each possible move in a new recursion. Return the minimum value of all of those
+    # 3. Stop each recursion if:
+    #       a) hitting a position you've already seen
+    #       b) finding the exit
+    # 4. Return total cost ( Each step costs 1 )
+
+    if lowest_costs is None:
+        lowest_costs = np.zeros((len(matrix), len(matrix[0])))
+
+    possible_costs = []
+    for move_str in possible_moves[pos[0]][pos[1]]:
+        new_cost = cost
+        move = MOVES[move_str]
+        new_pos = add_pos(pos, move)
+
+        prev_lowest_cost = lowest_costs[new_pos[0]][new_pos[1]]
+        if prev_lowest_cost == 0 or new_cost < prev_lowest_cost:
+            lowest_costs[new_pos[0]][new_pos[1]] = new_cost
+        else:
+            # Some other path got here quicker, ignore this way
+            continue
+
+        if is_possible_move_2(pos, new_pos, matrix):  # and new_pos not in current_path:
+            new_cost += 1
+            # new_path = copy.deepcopy(current_path)
+            # new_path.append(new_pos)
+
+            if new_pos == goal:
+                possible_costs.append([new_cost])
+            else:
+                new_best_cost, lowest_costs = get_best_path_2(possible_moves, matrix, new_pos, goal, new_cost, lowest_costs)
+                if new_best_cost is not None:
+                    possible_costs.append(new_best_cost)
+    if possible_costs != []:
+        min_cost = get_min_cost_from_list(possible_costs)
+        return min_cost, lowest_costs
+    else:
+        return None, lowest_costs
+
+
 def print_moves(best_path, matrix):
     path_matrix = np.zeros((len(matrix), len(matrix[0])))
     for i, pos in enumerate(best_path):
@@ -174,6 +231,24 @@ def solve_part_1():
 @time_it
 def solve_part_2():
     print("Day 12 - Part 2")
+    lines = read_input_lines(INPUT_FILENAME, True)
+    s_pos = get_start_pos(lines)
+    start_positions = get_pos_for_char(lines, "a")
+    start_positions.append(s_pos)
+    end_pos = get_end_pos(lines)
+    possible_moves = calculate_possible_moves(lines, 2)
+    costs = []
+    lowest_costs = None
+    for start_pos in start_positions:
+        cost, lowest_costs = get_best_path_2(possible_moves, lines, end_pos, start_pos)
+        if cost is not None:
+            print(f"Cost {cost[0]} for starting in pos {start_pos}")
+            costs.append(cost[0])
+        else:
+            print(f"No path for starting in pos {start_pos}")
+    print(f"The lowest cost was {min(costs)}")
+
+
 
 
 if __name__ == "__main__":
